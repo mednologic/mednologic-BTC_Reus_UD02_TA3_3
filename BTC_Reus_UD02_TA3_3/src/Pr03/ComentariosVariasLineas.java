45e691f03bfe63cb98cd07e17081f6b4ccced8f8
mednologic <mednologic@gmail.com>
2022-04-07 11:54:30 +0200

    "Structure and files created and operative"

---
+++ BTC_Reus_UD02_TA3_3/src/Pr03/ComentariosVariasLineas.java
@@ -0,0 +1,24 @@
+package Pr03;
+
+/**
+ * 
+ * @author joseporiollopezbosch
+ *
+ */
+
+public class ComentariosVariasLineas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		  System.out.println("Hola");
+		  System.out.println("Adios");
+		  System.out.println("Cucu");
+		*/
+		
+		for(int i=0;i<6;/* ahora viene el paso */ i++) {
+			System.out.print(i+" ");
+		}
+		System.out.println("FIN DE PROGRAMA");
+	}
+}
